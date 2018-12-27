42033d5d7511845eb62387a2322954ad0ed031e6
Francis Programmer <francisprogrammer@Franiciss-MBP.home>
2018-12-27 00:44:40 +0000

    stuff to upload

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -22,14 +22,14 @@ namespace api.Controllers
         {
             _mailServerConfig = mailServerConfig.Value;
         }
-        
+
         [HttpPost]
         public async Task EmailRandomNames(Range range, string email = "test@fake.com")
         {
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
             message.To.Add(new MailboxAddress("", email));
-            message.Subject = "Here are your random names";
+            message.Subject = "Hi Here are your random names";
 
             message.Body = new TextPart("plain")
             {
